4eef31ef33de6e4d3019a44a88c31ed0c2c3d3b5
Pranay HATTARAKAL <pranay.hattarakal@legrand.com>
2025-11-25 17:23:53 +0530

    Vm-1

--- virtualmachine/vm-setup.ts
+++ virtualmachine/vm-setup.ts
@@ -50,6 +50,9 @@ export class VirtualMachineManager {
     async cleanupVmIfPresent() {
         this.log(`--- Checking for existing VM '${this.config.name}' ---`);
         console.log(await this.execute(`ls -ld /var/lib/libvirt/images`));
+        console.log(await this.execute(`whoami`));
+        console.log(await this.execute(`groups`));
+        console.log(await this.execute(`getent group libvirt`));
         const listOutput = await this.execute(`virsh list --all`).catch(() => "");
         const vmExists = listOutput.includes(this.config.name);
 
